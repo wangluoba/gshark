a7bd786217c5415fc1838d9bae8c9c014dd50958
wangluoba <49388444+wangluoba@users.noreply.github.com>
2024-08-09 10:48:02 +0800

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo 'guest01:XIONGHUI123' | sudo chpasswd
-sudo usermod -aG wheel guest01
+
+# 添加guest02用户
+sudo adduser guest02 --disabled-password --gecos ""
+
+# 修改guest02用户密码
+echo 'guest02:XIONGHUI123' | sudo chpasswd
+
+# 将guest02用户添加到wheel组
+sudo usermod -aG wheel guest02
